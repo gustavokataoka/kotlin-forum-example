cffc13e275f684165a3711f085aa72a9e7c8f51b
Gustavo Kataoka <gustavo.kataoka@ufms.br>
2024-07-07 11:10:03 -0400

    initial commit

---
+++ src/main/kotlin/br/gk/forum_demo/ForumDemoApplication.kt
@@ -0,0 +1,11 @@
+package br.gk.forum_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumDemoApplication>(*args)
+}
---
+++ src/test/kotlin/br/gk/forum_demo/ForumDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.gk.forum_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
